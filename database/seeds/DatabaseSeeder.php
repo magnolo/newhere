c01a231fa74393f891a4917c77bc30419049ad90
Martin Ras <martin@x8x.at>
2016-04-30 20:38:52 +0200

    - db seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+        $this->call(LanguageTableSeeder::class);
+        $this->call(FilterSeeder::class);
+        $this->call(CategoryTableSeeder::class);
     }
 }
