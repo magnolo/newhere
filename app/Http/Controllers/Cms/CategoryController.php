8fdc607e6fa1cf2ee6739b49a0bbc39d70d800ee
Martin Ras <martin@x8x.at>
2016-05-21 01:18:35 +0200

    - added move method - take care on sortindex where the parent of a category can change or a new category is added - order categories by sortindex

--- app/Http/Controllers/Cms/CategoryController.php
+++ app/Http/Controllers/Cms/CategoryController.php
@@ -12,18 +12,20 @@ use Illuminate\Http\Request,
 
 class CategoryController extends Controller
 {
-    public function __construct(){
+    public function __construct()
+    {
         App::setLocale(app('request')->header('language'));
     }
 
     public function index(Request $request)
     {
-        if($request->get('all')){
-          $categories = Category::withTranslation()->orderBy('id', 'ASC')->get();
-        }
-        else{
-          $categories = Category::where('parent_id',null)->withTranslation()->orderBy('id', 'ASC')->get();
-          $categories->load('children');
+        if ($request->get('all')) {
+            $categories = Category::withTranslation()->orderBy('sortindex', 'ASC')->get();
+        } else {
+            $categories = Category::where('parent_id', null)->withTranslation()->orderBy('sortindex', 'ASC')->get();
+            $categories->load(['children' => function ($query) {
+                $query->orderBy('sortindex', 'ASC');
+            }]);
         }
 
         return response()->json($categories);
@@ -33,8 +35,8 @@ class CategoryController extends Controller
     {
         $category = Category::findOrFail((int)$id);
         $languages = Language::where('enabled', true)->get();
-        foreach($languages as $language){
-           $category->translate($language->language);
+        foreach ($languages as $language) {
+            $category->translate($language->language);
         }
         return response()->json($category);
     }
@@ -42,7 +44,7 @@ class CategoryController extends Controller
     public function create(Request $request)
     {
         $this->validate($request, [
-            'language'    => 'required|min:2|max:2',
+            'language' => 'required|min:2|max:2',
             'icon' => 'required|min:1|max:20',
             'title' => 'required|max:255',
             'description' => 'required'
@@ -54,16 +56,19 @@ class CategoryController extends Controller
         $category = new Category();
         $category->icon = $request->get('icon');
         $category->parent_id = $request->get('parent_id');
+        $category->sortindex = 999;
         $category->save();
 
         $category->translateOrNew($locale)->title = $request->get('title');
         $category->translateOrNew($locale)->description = $request->get('description');
         $category->save();
 
-        if($locale !== "de"){
+        if ($locale !== "de") {
             $category->translateOrNew('de')->title = $request->get('title');
             $category->save();
         }
+
+        $this->recreateSortIndex($request->get('parent_id'));
 
         DB::commit();
 
@@ -83,33 +88,135 @@ class CategoryController extends Controller
             return response()->error('Category not found', 404);
         }
 
+        $parentCategory = null;
+        if ($request->get('parent_id')) {
+            $parentCategory = Category::find((int)$request->get('parent_id'));
+            if (!$parentCategory) {
+                return response()->error('Category not found', 404);
+            }
+        }
+
+        $changeParent = false;
+        if (($parentCategory && $category->parent_id && $parentCategory->id != $category->parent_id)
+            || (!$parentCategory && $category->parent_id)
+        ) {
+            $changeParent = true;
+            $oldParent = $category->parent_id;
+        }
+
         DB::beginTransaction();
 
-        $locale = $request->get('language');
-        $category->icon = $request->get('icon');
-        $category->parent_id = $request->get('parent_id');
+        $this->recreateSortIndex(
+            ($parentCategory ? $parentCategory->id : null),
+            ($changeParent ? 999 : null)
+        );
 
-        foreach($request->get('translations') as $transalation){
-          $category->translateOrNew($transalation['locale'])->title = $transalation['title'];
-          $category->translateOrNew($transalation['locale'])->description = $transalation['description'];
+        $category->icon = $request->get('icon');
+        $category->parent_id = ($parentCategory ? $parentCategory->id : null);
+        if ($changeParent) {
+            $category->sortindex = 999;
+        }
+
+        /**
+         * @todo versioning!
+         */
+        foreach ($request->get('translations') as $transalation) {
+            $category->translateOrNew($transalation['locale'])->title = $transalation['title'];
+            $category->translateOrNew($transalation['locale'])->description = $transalation['description'];
 
         }
         $category->save();
+
+        if ($changeParent) {
+            // do it a 2nd time to fix sortindex
+            $this->recreateSortIndex(
+                ($parentCategory ? $parentCategory->id : null)
+            );
+
+            $this->recreateSortIndex($oldParent);
+        }
 
         DB::commit();
 
         return response()->success(compact('category'));
     }
 
-    public function toggleEnabled(Request $request, $id){
-      $this->validate($request, [
-          'enabled'  => 'required',
-      ]);
+    public function move(Request $request, $id)
+    {
+        $this->validate($request, [
+            'sortindex' => 'required|int',
+            'parent' => 'int'
+        ]);
 
-      $category = Category::findOrFail((int)$id);
-      $category->enabled = $request->get('endabled');
+        $category = Category::find((int)$id);
+        if (!$category) {
+            return response()->error('Category not found', 404);
+        }
 
-      return response()->success(compact('category'));
+        $parentCategory = null;
+        if ($request->get('parent')) {
+            $parentCategory = Category::find((int)$request->get('parent'));
+            if (!$parentCategory) {
+                return response()->error('Category not found', 404);
+            }
+        }
 
+        $changeParent = false;
+        if (($parentCategory && $category->parent_id && $parentCategory->id != $category->parent_id)
+            || (!$parentCategory && $category->parent_id)
+        ) {
+            $changeParent = true;
+            $oldParent = $category->parent_id;
+        }
+
+        $newSortIndex = (int)$request->get('sortindex');
+
+        DB::beginTransaction();
+
+        $this->recreateSortIndex(
+            ($parentCategory ? $parentCategory->id : null),
+            $newSortIndex
+        );
+
+        $category->parent_id = ($parentCategory ? $parentCategory->id : null);
+        $category->sortindex = $newSortIndex;
+        $category->save();
+
+        if ($changeParent) {
+            $this->recreateSortIndex($oldParent);
+        }
+
+        DB::commit();
+
+        return response()->json($category);
+    }
+
+    public function toggleEnabled(Request $request, $id)
+    {
+        $this->validate($request, [
+            'enabled' => 'required',
+        ]);
+
+        $category = Category::findOrFail((int)$id);
+        $category->enabled = $request->get('endabled');
+
+        return response()->success(compact('category'));
+
+    }
+
+    private function recreateSortIndex($parentId = null, $newSortIndex = null)
+    {
+        $categoryItems = Category::where('parent_id', $parentId)->orderBy('sortindex', 'ASC')->get();
+        $currentSortIndex = 0;
+        foreach ($categoryItems as $categoryItem) {
+            if ($newSortIndex !== null && $currentSortIndex == $newSortIndex) {
+                $currentSortIndex++;
+            }
+            if ($categoryItem->sortindex != $currentSortIndex) {
+                $categoryItem->sortindex = $currentSortIndex;
+                $categoryItem->save();
+            }
+            $currentSortIndex++;
+        }
     }
 }
