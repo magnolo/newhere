4713b94771bc418e296ab3c61ab25e6f93098994
Konrad Pozniak <e0925996@student.tuwien.ac.at>
2016-06-10 16:03:20 +0200

    merged offer list and offer details

--- angular/app/components/app-categories-content-sub/app-categories-content-sub.component.js
+++ angular/app/components/app-categories-content-sub/app-categories-content-sub.component.js
@@ -2,24 +2,38 @@ class AppCategoriesContentSubController {
     constructor(CategoryService, OfferService, MapService, $state) {
         'ngInject';
 
-
+        this.showMap = false;
+        this.showOffers = false;
         this.category = {};
         this.CategoryService = CategoryService;
         this.OfferService = OfferService;
         this.MapService = MapService;
+        this.state = $state;
+        this.offersCallback = (offers) => {
+            this.offers = offers;
+            this.MapService.setMarkers(offers);
+        };
         this.CategoryService.bySlug($state.params.slug, (category) => {
             this.category = category;
-            this.CategoryService.getOffers(this.MapService.setMarkers);
+            if (!this.category.children.length) {
+                this.showOffers = true;
+            }
+            this.CategoryService.getOffers(this.offersCallback);
         });
 
 
+
     }
-    prepareLink(category){
-      if(category.children.length){
-        return 'app.start.categories.sub({slug:'+category.slug+'})';
-      }
-      return '-';
+
+    toggleMap() {
+        this.showMap = !this.showMap;
     }
+
+    showOffer(id) {
+        this.state.go('app.start.detail',{id:id});
+    }
+
+
 }
 
 export const AppCategoriesContentSubComponent = {
