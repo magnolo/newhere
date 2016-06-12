3ba41c0532c418d9bb68ee57f5b54dcb4aad7767
Martin Ras <martin@x8x.at>
2016-06-12 18:47:11 +0200

    - angular modules for translate - basic angular translate config

---
+++ angular/config/translate.config.js
@@ -0,0 +1,11 @@
+export function TranslateConfig($translateProvider){
+    'ngInject';
+
+    $translateProvider.useStaticFilesLoader({
+        prefix: '/translations/',
+        suffix: '.json'
+    });
+
+    $translateProvider.preferredLanguage('en');
+    $translateProvider.fallbackLanguage('de');
+}
